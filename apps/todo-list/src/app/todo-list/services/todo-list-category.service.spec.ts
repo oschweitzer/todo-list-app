8108d79ca8052f61b4489424275cfbbfcb3341bc
Oli1810 <olivier.sch68@gmail.com>
2020-02-16 18:42:41 +0100

    feat(todo-list/services): add category service

---
+++ apps/todo-list/src/app/todo-list/services/todo-list-category.service.spec.ts
@@ -0,0 +1,12 @@
+import { TestBed } from '@angular/core/testing';
+
+import { TodoListCategoryService } from './todo-list-category.service';
+
+describe('TodoListCategoryService', () => {
+  beforeEach(() => TestBed.configureTestingModule({}));
+
+  it('should be created', () => {
+    const service: TodoListCategoryService = TestBed.get(TodoListCategoryService);
+    expect(service).toBeTruthy();
+  });
+});
---
+++ apps/todo-list/src/app/todo-list/services/todo-list-category.service.ts
@@ -0,0 +1,22 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { DataMessage } from '@todo-list-app/api-interfaces';
+import { CategoryEntity } from '@todo-list-app/models';
+import { Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
+import { environment } from '../../../environments/environment';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class TodoListCategoryService {
+  constructor(
+    private readonly httpClient: HttpClient,
+  ) { }
+
+  getAll(): Observable<CategoryEntity[]> {
+    return this.httpClient.get<DataMessage<CategoryEntity>>(`${environment.api.baseUrl}/api/categories`).pipe(
+      map(response => response.data)
+    );
+  }
+}
